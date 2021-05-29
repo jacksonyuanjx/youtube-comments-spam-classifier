7bc2cb2a2ba8186b6c4f09b455d3fb221122a1f9
jacksonyuanjx <jacksonyuanjx@hotmail.com>
2021-05-28 20:24:29 -0700

    basic flask API for predictions endpoint

---
+++ server.py
@@ -0,0 +1,23 @@
+import connexion
+
+# Create the application instance
+app = connexion.App(__name__, specification_dir="./")
+
+# Read the swagger.yml file from the `specification_dir` to configure the endpoints
+app.add_api('swagger.yml')
+
+
+@app.route('/')
+def home():
+    """
+    This function just responds to the browser URL localhost:5000/
+    
+    :return: the rendered template 'home.html'
+    """
+    return "Endpoints are online."
+
+
+# handling when app is run from cmd line
+if __name__ == '__main__':
+    print("running from cmd line")
+    app.run(port=5000, debug=True)
